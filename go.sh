edc425fe2c5c10021e126d9b61f4530d43ce4387
mrzhenggang <mrzhenggang@foxmail.com>
2020-01-03 23:39:31 +0800

    rebuilding site 2020年01月 3日 23:39:30

---
+++ go.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to Gitee...\033[0m"
+
+git add .
+git config --global core.autocrlf false
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+#git push coding master
+
