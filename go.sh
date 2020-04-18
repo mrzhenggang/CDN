1d254ef56aefcf41c971a0a1777466d7c3873ad4
mrzhenggang <mrzhenggang@foxmail.com>
2020-04-18 15:51:44 +0800

    rebuilding site 2020年04月18日 15:51:44

--- go.sh
+++ go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to Gitee...\033[0m"
+echo -e "\033[0;32mDeploying updates to Github...\033[0m"
 
 git add .
 git config --global core.autocrlf false
