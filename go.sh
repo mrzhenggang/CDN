5299b6e6fbebeab04677b1c2fc90637888b70c3b
zhenggang <mrzhenggang@foxmail.com>
2021-06-29 15:08:02 +0800

    rebuilding site Tue Jun 29 15:08:02     2021

--- go.sh
+++ go.sh
@@ -4,6 +4,8 @@ echo -e "\033[0;32mDeploying updates to Github...\033[0m"
 
 git add .
 git config --global core.autocrlf false
+git config --global http.sslVerify "false"
+
 # Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
